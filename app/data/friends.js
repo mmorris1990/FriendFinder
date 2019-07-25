c3a83127df18ffa5ad1e758d114fb0a955aa9016
Michael Morris <mmorris1990@gmail.com>
2019-07-25 18:07:49 -0500

    built empty array for results in friends.js and server basics in server.js

---
+++ app/data/friends.js
@@ -0,0 +1,5 @@
+// Empty array to store survey results
+var surveyAnswers = [];
+
+// Export the array so other files can access
+module.exports = surveyAnswers;
---
+++ server.js
@@ -0,0 +1,20 @@
+// Use Express Module
+var express = require("express");
+
+// Declares out server as an express server and sets port(Heroku or 8080)
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets express up to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Require the two routing .js files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Start the server!
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
